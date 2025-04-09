4013877bc91aa07900676440d1eb1ed3653d616f
LosAllen <lincolnjallen@gmail.com>
2025-04-09 15:54:41 -0500

    fixed comments

--- models/comment.js
+++ models/comment.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 
 const CommentSchema = new mongoose.Schema({
     recipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe', required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     text: { type: String, required: true },
-    rating: { type: Number, min: 1, max: 5 },
     dateCreated: { type: Date, default: Date.now }
 });
 
